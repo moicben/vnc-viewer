f0a8dda47ee1c585014e8ced5fc8e26d3436be11
moicben <moicben@email.com>
2026-01-25 10:05:53 +0800

    up

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,8 +5,9 @@ export default function Document() {
         <Html lang="fr">
             <Head>
                 <meta name="theme-color" content="#667eea" />
-                <link rel="shortcut icon" href="/favicon.png" />
-                <link rel="icon" href="/favicon.png" />
+                <link rel="icon" type="image/png" href="/favicon.png" />
+                <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+                <link rel="apple-touch-icon" href="/favicon.png" />
                 <link rel="preload" as="image" href="/favicon.png" />
 
             </Head>
